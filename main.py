2998800519b3400e0c04eee9d8bc729ade27e84c
sgustjd2 <shin1133@naver.com>
2023-03-15 13:28:45 +0900

    master 2

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ dst1, dst2, dst3 = None,None,None
 
 src = cv2.imread("c:/imgs/picture01.jpg")
 
+dst1 = cv2.grayScale(src)
+dst2 = cv2.cannyEdge(src)
+
+
 cv2.imshow('Src',src)
+cv2.imshow('dts1',dst1)
+cv2.imshow('dts2',dst2)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows
